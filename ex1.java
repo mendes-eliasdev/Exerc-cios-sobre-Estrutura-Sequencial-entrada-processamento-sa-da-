168244736677008f23d644950566eeab7d3337cf
mendes-eliasdev <111157933+mendes-eliasdev@users.noreply.github.com>
2022-08-12 20:45:45 -0300

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -1,4 +1,12 @@
-import java.util.Locale;
+Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
+casas decimais conforme exemplos.
+Fórmula da área: area = π . raio2
+Considere o valor de π = 3.14159
+Entrada:2.00
+Saida :A=12.5664
+
+
+import java.util.Locale; 
 import java.util.Scanner;
 
 public class Main1 {
